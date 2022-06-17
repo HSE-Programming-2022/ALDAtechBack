1af470e43f1f8999feac83300b263cc4fdf3bbd6
Sinedzuk <deniskuz@pm.me>
2022-06-17 12:52:26 +0300

    Функционал получения сообщейний от пользователя и их обработки, реализован wair for событий

--- AldaTech.API/BotCore/TelegramBotWrapper.cs
+++ AldaTech.API/BotCore/TelegramBotWrapper.cs
@@ -1,1 +1,135 @@
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+// var bot = new TelegramBot("5001678276:AAH0MgT6aPNR7pkOGeD5eYFxEsJI3sgE4WA");
+// Console.ReadLine();
+
+
 namespace AldaTech_api.BotCore;
+
+
+public class MessageListener
+{
+    public TaskCompletionSource<Message> Task { get; set; }
+    private Func<Message, bool> Check;
+
+    public MessageListener(Func<Message, bool> check)
+    {
+        this.Check = check;
+        this.Task = new TaskCompletionSource<Message>();
+    }
+
+    public bool ListensTo(Message message) => Check(message);
+}
+
+public class TelegramBotWrapper
+{
+    private TelegramBotClient _botClient;
+    private string _token;
+    private CancellationTokenSource _cts;
+    private ReceiverOptions _receiverOptions;
+    private List<MessageListener> _listeners;
+
+    public TelegramBotWrapper(string token)
+    {
+        _token = token;
+        _botClient = new TelegramBotClient(token);
+        _cts = new CancellationTokenSource();
+        
+        _receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+        };
+        
+        _botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: _receiverOptions,
+            cancellationToken: _cts.Token
+        );
+
+        _listeners = new List<MessageListener>();
+    }
+
+    public void Destroy()
+    {
+        _cts.Cancel();
+    }
+    
+    public TaskCompletionSource<Message> WaitFor(Func<Message, bool> check)
+    {
+        var listener = new MessageListener(check);
+        _listeners.Add(listener);
+        return listener.Task;
+    }
+    
+    async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Only process Message updates: https://core.telegram.org/bots/api#message
+        if (update.Type != UpdateType.Message)
+            return;
+        if (update.Message is null)
+            return;
+        // Only process text messages
+        if (update.Message!.Type != MessageType.Text)
+            return;
+
+        Message message = update.Message;
+        Console.WriteLine("Got message" + message.Text);
+        foreach (var listener in _listeners)
+        {
+            if (listener.ListensTo(message))
+            {
+                listener.Task.TrySetResult(message);
+                _listeners.Remove(listener);
+                Console.WriteLine("Listener worked");
+                return;
+            }
+        }
+        
+        NewDialog(message);
+    }
+    
+    Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
+
+    private async Task NewDialog(Message message)
+    {
+        var chatId = message.Chat.Id;
+        var messageText = message.Text;
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+        // Echo received message text
+        await _botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Ener age",
+            cancellationToken: _cts.Token);
+        var msg = await (WaitFor(x => x.Chat.Id == chatId).Task);
+        var age = msg.Text;
+        await _botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Enter name",
+            cancellationToken: _cts.Token);
+        msg = await (WaitFor(x => x.Chat.Id == chatId).Task);
+        var name = msg.Text;
+        
+        await _botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: age + " - " + name,
+            cancellationToken: _cts.Token);
+    }
+}
