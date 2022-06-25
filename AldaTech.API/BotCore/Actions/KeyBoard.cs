7b0d2ca0aac2826980059a9f6b4a637a6cc7cc91
Sinedzuk <deniskuz@pm.me>
2022-06-25 17:33:50 +0300

    Сделал присвоение кнопок для клавиатуры для передаваемых значений _options

--- AldaTech.API/BotCore/Actions/KeyBoard.cs
+++ AldaTech.API/BotCore/Actions/KeyBoard.cs
@@ -7,25 +7,41 @@ public class KeyBoard : IBotAction
 {
     private IBotClient _botClient;
     [JsonProperty]
-    private ReplyKeyboardMarkup _keys;
+    private List<string> _options;
     [JsonProperty]
     private string _text;
     private long _chatId;
     
-    public KeyBoard(string text, ReplyKeyboardMarkup keys, IBotClient botClient, long chatId)
+    public KeyBoard(string text, List<string> options, IBotClient botClient, long chatId)
     {
         _text = text;
-        _keys = keys;
+        _options = options;
         _botClient = botClient;
         _chatId = chatId;
     }
     
+    
+    
     public async Task<ActionExecutionResult> Run(BotUserContext ctx)
     {
+        var keyboard = new KeyboardButton[_options.Count];
+        for (int i = 0; i < keyboard.Length; i++)
+        {
+            keyboard[i] = _options[i];
+        }
+        
+        ReplyKeyboardMarkup keys = new(new []
+        {
+            keyboard ,
+        })
+        {
+            ResizeKeyboard = true
+        };
+        
         _botClient = ctx.BotClient;
         _chatId = ctx.ChatId;
         
-        await _botClient.SendKeyBoardAsync(_chatId, _text, _keys);
+        await _botClient.SendKeyBoardAsync(_chatId, _text, keys);
         return new ActionExecutionResult(ActionExecutionStatus.RunNext);
     }
 }
